bd62e121e0ed045d52eeddc3a5c436e0825d4c4f
Alex760164 <titorenkosanya@gmail.com>
2020-09-30 09:40:20 +0300

    Добавление домашнего задания №22 (пересдача)

--- home_work_22/home_work_22.py
+++ home_work_22/home_work_22.py
@@ -7,14 +7,12 @@
 def is_year_leap(y):
     if y % 4 != 0:
         return False
+    elif y % 100 != 0:
+        return True
+    elif y % 400 != 0:
+        return False
     else:
-        if y % 100 != 0:
-            return True
-        else:
-            if year % 400 != 0:
-                return False
-            else:
-                return True
+        return True
 
 
 year = int(input('Введите год для проверки: '))
