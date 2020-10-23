f409649f59cedc07ac5706438b89965016eec9b3
D09-Titorenko <D09-Titorenko@1553-10480866.kiev.odessa.sta>
2020-10-23 10:09:02 +0300

    Добавление домашнего задания №34

--- home_work_34/home_work_34.py
+++ home_work_34/home_work_34.py
@@ -4,42 +4,32 @@
 Так же, записать в новый файл всех учащихся в формате "Фамилия И.    Ср. балл":
 """
 
-
-def one_sub(text):
-    text.split()
-    result = ' '.join(text.split())
-    return result
-
-
 src = open('src.txt', 'r', encoding='utf-8')
 new_src = open('new_src.txt', 'w', encoding='utf-8')
+format_print = '{fio:<25} {avr_ball:>10.2}'
 
-tmp_list = []
-new_list = []
+total = 0
+count = 0
+print('ОТВЕТ:\nСписок учеников у которых средний бал меньше 5:')
+
 while True:
     data = src.readline()
     if data:
-        tmp_list = one_sub(data).split(' ')
+        tmp_list = data.strip('\n').split()
         full_name = tmp_list[0]
         name = tmp_list[0][0] + '.'
         first_name = tmp_list[1]
-        sum_ball = sum(list(map(int, tmp_list[2:])))
-        col_ball = len(list(map(int, tmp_list[2:])))
-        avr_ball = round(sum_ball/col_ball, 2)
-        new_src.write(first_name + ' ' + name + '\t' + str(avr_ball) + '\n')
-        new_list.append([first_name, full_name, avr_ball])
+        avr_ball = sum(list(map(int, tmp_list[2:])))/len(tmp_list[2:])
+        res_for_file = format_print.format(fio=first_name + ' ' + name, avr_ball=avr_ball)
+        res_on_screen = format_print.format(fio=first_name + ' ' + full_name, avr_ball=avr_ball)
+        new_src.write(res_for_file + '\n')
+        if avr_ball < 5:
+            print('\t' + res_on_screen)
+        total += avr_ball
+        count += 1
     else:
         break
+
+print('\nСредний балл по классу: ', round(total / count, 2))
+new_src.close()
 src.close()
-
-print('ОТВЕТ:\nСписок учеников у которых средний бал меньше 5:')
-for i in range(len(new_list)):
-    if new_list[i][2] < 5:
-        print('\t' + new_list[i][0] + ' ' + new_list[i][1] + ' ' + str(new_list[i][2]))
-
-print('\nСредний балл по классу: ', end='')
-summ = 0
-for i in range(len(new_list)):
-    summ = summ + new_list[i][2]
-group_avr_ball = round(summ / len(new_list), 2)
-print(group_avr_ball)
