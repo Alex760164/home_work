bcd069b58a831ff3779bdcfe7963869ca8821f24
D09-Titorenko <D09-Titorenko@1553-10480866.kiev.odessa.sta>
2020-10-29 14:00:30 +0200

    Добавление домашнего задания №36

--- home_work_36/home_work_36.py
+++ home_work_36/home_work_36.py
@@ -7,22 +7,24 @@
 """
 
 
-class Group:
-    class Student:
-        def __init__(self, name, age, grades):
-            self.name = name
-            self.age = age
-            self.grades = grades
+class Student:
+    def __init__(self, name, age, grades):
+        self.name = name
+        self.age = age
+        self.grades = grades
 
-        def information_of_student(self):
-            format_print = '| {fio:<30}|{age:^10}|  {grades:^48}|'
-            return format_print.format(fio=self.name, age=str(self.age), grades=self.grades)
+    def information_of_student(self):
+        format_print = '| {fio:<30}|{age:^10}|  {grades:^48}|'
+        return format_print.format(fio=self.name, age=str(self.age), grades=self.grades)
+
+
+class Group:
 
     def __init__(self):
         self.list = []
 
     def add_record(self, name, age, grades):
-        self.list.append(Group.Student(name, age, grades).information_of_student())
+        self.list.append(Student(name, age, grades).information_of_student())
 
     def print_of_screen(self):
         print('\n'.join(item for item in self.list))
